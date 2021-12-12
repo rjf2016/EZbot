7809e2ec52bc030af6b9f7473fb60c2e182b6742
Rick Fahey <rickfahey81@gmail.com>
2021-12-12 15:59:55 -0500

    wrap command.run execution in a try/catch block

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -2,16 +2,29 @@ import { CommandInteractionOptionResolver } from 'discord.js'
 import { client } from '..'
 import { Event } from '../structures/Event'
 import { ExtendedInteraction } from '../typings/Command'
+import { isProd } from '../util/validateEnv'
 
 export default new Event('interactionCreate', async (interaction) => {
   if (interaction.isCommand()) {
     const command = client.commands.get(interaction.commandName)
-    if (!command) return interaction.followUp('That command does not exist')
+    if (!command)
+      return await interaction.reply({
+        content: 'That command does not exist',
+        ephemeral: isProd(),
+      })
 
-    command.run({
-      args: interaction.options as CommandInteractionOptionResolver,
-      client,
-      interaction: interaction as ExtendedInteraction,
-    })
+    try {
+      command.run({
+        args: interaction.options as CommandInteractionOptionResolver,
+        client,
+        interaction: interaction as ExtendedInteraction,
+      })
+    } catch (error) {
+      console.error(error)
+      return await interaction.reply({
+        content: "❌ Uh oh, I've encountered an unexpected error 🤕",
+        ephemeral: isProd(),
+      })
+    }
   }
 })

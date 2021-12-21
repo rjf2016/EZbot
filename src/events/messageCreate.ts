b9349793cdbf798ef7d63d4ac46d8428950b1258
Rick Fahey <rickfahey81@gmail.com>
2021-12-20 23:39:48 -0500

    revert clientEvent dir changes

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -3,6 +3,11 @@ import { Event } from '../structures/Event'
 import { cyan } from 'chalk'
 
 export default new Event('messageCreate', async (message: Message) => {
+  if (message.author.id === '282859044593598464') {
+    await message.react('💩')
+    await message.react('🤖')
+    return
+  }
   if (message.author.bot || !message.guild) return
   console.log(cyan(message.author.username) + ':', message.content)
 })

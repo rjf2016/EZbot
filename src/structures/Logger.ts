592c979d1ba91f77a42cc5d52cc904b6e3125f14
Rick Fahey <rickfahey81@gmail.com>
2022-02-17 22:40:13 -0500

    fix pino-pretty

--- src/structures/Logger.ts
+++ src/structures/Logger.ts
@@ -1,14 +1,5 @@
 import pino, { BaseLogger } from 'pino'
 
-const logger: BaseLogger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:h:MM:ss TT',
-      ignore: 'pid,hostname',
-    },
-  },
-})
+const logger: BaseLogger = pino()
 
 export default logger
